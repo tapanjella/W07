bda4eb5decea1f0b58835a60827ab21343f03ea9
Tapan Jella <tapanjella1@gmail.com>
2017-10-12 18:43:40 -0500

    trfrt

---
+++ gbapp.js
@@ -0,0 +1,56 @@
+var path = require("path")
+var express = require("express")
+var logger = require("morgan")
+var bodyParser = require("body-parser") // simplifies access to request body
+
+
+
+var app = express()  // make express app
+var http = require('http').Server(app)  // inject app into the server
+
+// 1 set up the view engine
+app.set("views", path.resolve(__dirname, "views")) // path to views
+app.set("view engine", "ejs") // specify our view engine
+
+// 2 create an array to manage our entries
+var entries = []
+app.locals.entries = entries // now entries can be accessed in .ejs files
+
+// 3 set up an http request logger to log every request automagically
+app.use(logger("dev"))     // app.use() establishes middleware functions
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/assets'))
+
+// 4 handle http GET requests (default & /new-entry)
+app.get("/", function (request, response) {
+  response.render("index")
+})
+app.get("/new-entry", function (request, response) {
+  response.render("new-entry")
+})
+
+// 5 handle an http POST request to the new-entry URI 
+app.post("/new-entry", function (request, response) {
+  console.log(request.body);  
+  if (!request.body.title || !request.body.body) {
+    response.status(400).send("Entries must have a title and a body.")
+    return
+  }
+  entries.push({  // store it
+    title: request.body.title,
+    content: request.body.body,
+    published: new Date()
+  })
+  response.redirect("/")  // where to go next? Let's go to the home page :)
+})
+
+// if we get a 404 status, render our 404.ejs view
+app.use(function (request, response) {
+  response.status(404).render("404")
+})
+
+
+// Listen for an application request on port 8081
+http.listen(8081, function () {
+  console.log('Guestbook app listening on http://127.0.0.1:8081/')
+})
